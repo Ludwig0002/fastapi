177172e5a531d0d2b9e32c3e5238d3fb6a0a95aa
Ludwig0002 <lj.englbrecht@gmail.com>
2025-07-23 21:54:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from typing import Optional
-
 from fastapi import FastAPI
+
+# app = FastAPI()
+# @app.get("/")
+# async def root():
+#    return {"message": "Hello World"}
+#
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#    return {"item_id": item_id, "q": q}
+
+
+
+# from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import asyncio
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/sse")
+async def sse_endpoint():
+    async def event_generator():
+        for i in range(10):
+            yield f"data: Nachricht {i}\n\n"
+            await asyncio.sleep(1)
+    return StreamingResponse(event_generator(), media_type="text/event-stream")
